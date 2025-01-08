c4fb2db255fe9020fbfc1b98dc548f2664992053
AVISHERE <akashvyas2006@gmail.com>
2025-01-08 19:57:33 +0530

    added routes

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import { userRoutes } from './router/user.router.js';
+import { expenseRouter } from './router/expense.router.js';
+import jwt from 'jsonwebtoken'
+import { refreshRefreshToken } from './controller/user.controller.js';
+const app = express();
+
+app.use(express.json());
+
+
+app.get('/',(req,res)=>{
+    res.send("hello world");
+});
+app.use('/v1/user',userRoutes)
+app.use('/v1/expense',expenseRouter)
+app.get('/v1/refresh',refreshRefreshToken);
+
+
+app.listen(8080,()=>{
+    console.log("port listing on \nhttp://localhost:8080");
+})
+
