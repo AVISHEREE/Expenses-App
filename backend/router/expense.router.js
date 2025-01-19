fa2c161d5737f202d51e67f3be38741c73a22633
AVISHERE <akashvyas2006@gmail.com>
2025-01-19 22:32:30 +0530

    changes in expenses routes

--- backend/router/expense.router.js
+++ backend/router/expense.router.js
@@ -8,8 +8,8 @@ import { verifyJwtToken } from "../middleware/verifyJwtToken.js";
 const router = Router();
 
 router.route('/add-expense').post(verifyJwtToken,addExpense);
-router.route('/get-expense/:id').get(verifyJwtToken,getAnExpense);
-router.route('/get-all-expenses').get(verifyJwtToken,getAllExpense);
+router.route('/get-expense/:id').post(verifyJwtToken,getAnExpense);
+router.route('/get-all-expenses').post(verifyJwtToken,getAllExpense);
 router.route('/delete-expense').delete(verifyJwtToken,deleteAnExpense);
 router.route('/update-expense').put(verifyJwtToken,updateAnExpense);
 
